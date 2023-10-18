deda0e42da64096ece80d2bce5a20e51deb59740
Dan Parkes <dan.parkes@withwise.com>
2023-10-18 23:22:27 +0100

    channel specific tracking, no last author send, disable and enable bot

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -22,14 +22,19 @@ const (
 )
 
 type State struct {
-	GuildList map[string]Guild
+	GuildList map[string]*Guild
 }
 
 type Guild struct {
 	ID             string
-	ActiveChannels map[string]struct{}
-	Count          int
-	HighestCount   int
+	ActiveChannels map[string]*Count
+}
+
+type Count struct {
+	ChannelID        string
+	Count            int
+	HighestCount     int
+	PreviousSenderID string
 }
 
 func main() {
@@ -37,7 +42,7 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	state := &State{
-		GuildList: make(map[string]Guild),
+		GuildList: make(map[string]*Guild),
 	}
 
 	client, err := disgo.New(TOKEN,
@@ -70,36 +75,87 @@ func (s *State) onMessageCreate(event *events.MessageCreate) {
 		return
 	}
 
+	if event.Message.Content == "!cc help" {
+		event.Client().Rest().CreateMessage(event.ChannelID, discord.NewMessageCreateBuilder().SetContent("Just count! Use `!cc enable` and `!cc disable` to enable/disable the bot for specific channels.").Build())
+		return
+	}
+
+	currentGuildInfo, ok := s.GuildList[event.GuildID.String()]
+	if !ok {
+		currentGuildInfo = &Guild{
+			ID: event.GuildID.String(),
+			ActiveChannels: map[string]*Count{event.ChannelID.String(): {
+				ChannelID:        event.ChannelID.String(),
+				Count:            0,
+				HighestCount:     0,
+				PreviousSenderID: "",
+			}},
+		}
+	}
+	currentChannelInfo, ok := currentGuildInfo.ActiveChannels[event.ChannelID.String()]
+	if !ok {
+		currentGuildInfo.ActiveChannels[event.ChannelID.String()] = &Count{
+			ChannelID:        event.ChannelID.String(),
+			Count:            0,
+			HighestCount:     0,
+			PreviousSenderID: "",
+		}
+	}
+	if event.Message.Content == "!cc disable" {
+		event.Client().Rest().CreateMessage(event.ChannelID, discord.NewMessageCreateBuilder().SetContent("Counting disabled for the channel.").Build())
+		currentGuildInfo.ActiveChannels[currentChannelInfo.ChannelID].Count = -1
+		s.GuildList[event.GuildID.String()] = currentGuildInfo
+		return
+	}
+	if event.Message.Content == "!cc enable" {
+		event.Client().Rest().CreateMessage(event.ChannelID, discord.NewMessageCreateBuilder().SetContent("Counting enabled for the channel, starting at 0.").Build())
+		currentGuildInfo.ActiveChannels[currentChannelInfo.ChannelID].Count = 0
+		s.GuildList[event.GuildID.String()] = currentGuildInfo
+		return
+	}
+	if currentChannelInfo.Count == -1 {
+		return
+	}
+
 	countGiven, err := strconv.Atoi(event.Message.Content)
 	if err != nil {
 		return
 	}
-	currentGuildInfo, ok := s.GuildList[event.GuildID.String()]
-	if !ok {
-		currentGuildInfo = Guild{
-			ID:             event.GuildID.String(),
-			ActiveChannels: map[string]struct{}{event.ChannelID.String(): {}},
-			Count:          0,
-		}
-	}
 
-	if countGiven != currentGuildInfo.Count+1 {
+	if countGiven != currentChannelInfo.Count+1 || event.Message.Author.ID.String() == currentChannelInfo.PreviousSenderID {
 		str := "OOP! Count resetting, you're a dumbo!"
-		if currentGuildInfo.HighestCount < currentGuildInfo.Count {
-			str += fmt.Sprintf(" You got a new highest count of %v! Previously it was %v.", currentGuildInfo.Count, currentGuildInfo.HighestCount)
-			currentGuildInfo.HighestCount = currentGuildInfo.Count
-		} else {
-			str += fmt.Sprintf(" Highest count reached is still %v.", currentGuildInfo.HighestCount)
+
+		if countGiven != currentChannelInfo.Count+1 {
+			str += " You can't count!"
+		}
+		if event.Message.Author.ID.String() == currentChannelInfo.PreviousSenderID {
+			str += " You sent the last count!"
 		}
 
-		_, _ = event.Client().Rest().CreateMessage(event.ChannelID, discord.NewMessageCreateBuilder().SetContent(str).Build())
+		if currentChannelInfo.HighestCount < currentChannelInfo.Count {
+			str += fmt.Sprintf(" You got a new highest count of %v! Previously it was %v.", currentChannelInfo.Count, currentChannelInfo.HighestCount)
+			currentChannelInfo.HighestCount = currentChannelInfo.Count
+		} else {
+			str += fmt.Sprintf(" Highest count reached is still %v.", currentChannelInfo.HighestCount)
+		}
+
 		event.Client().Rest().AddReaction(event.ChannelID, event.MessageID, "🚫")
-		currentGuildInfo.Count = 0
+		_, _ = event.Client().Rest().CreateMessage(event.ChannelID, discord.NewMessageCreateBuilder().SetContent(str).Build())
+		currentChannelInfo.Count = 0
+		currentChannelInfo.PreviousSenderID = ""
 	} else {
 		event.Client().Rest().AddReaction(event.ChannelID, event.MessageID, "✅")
-		currentGuildInfo.Count += 1
+		currentChannelInfo.Count += 1
+		currentChannelInfo.PreviousSenderID = event.Message.Author.ID.String()
 	}
 
 	s.GuildList[event.GuildID.String()] = currentGuildInfo
 	log.Debugf("State Update: %+v", s)
+	log.Debugf("State Update: %+v", s.GuildList)
+	for k, g := range s.GuildList {
+		log.Debugf("State Update: %v, %+v", k, g)
+		for k2, c := range g.ActiveChannels {
+			log.Debugf("State Update: %v, %+v", k2, c)
+		}
+	}
 }
